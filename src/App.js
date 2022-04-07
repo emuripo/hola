dbf4ed3e9af97a4c10ac67da0a66e8aa8e4c3ce8
emuripo <esteban.murilloporras@gmail.com>
2022-04-07 00:45:05 -0600

    mi primer commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './styles/App.css';
+import Header from './components/header';
+import Dish from './components/dish';
+import React,{Component} from "react";
+
+
+class App extends Component{
+  dish = "perritos";
+  d = ["Pizza","Hambuerguesas","papas","Tincho"];
+  render (){
+    return (
+      <div className="App">
+        <Header></Header>
+        <Dish ></Dish>
+      </div>
+    );
+  }
+}
+
+export default App;
